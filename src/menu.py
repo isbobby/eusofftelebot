5d64fce5f44bab5823b7b5a97e410316069b0a39
isbobby <bobbyclex8@gmail.com>
2019-09-07 23:06:48 +0800

    added try catch for bot, edited csv error

--- src/menu.py
+++ src/menu.py
@@ -20,7 +20,13 @@ tomorrow = tomorrow_raw.strftime("%d/%m/%Y") """
 #these functions query and format menu data
 def get_today_breakfast(day):
     #converting pandas column object to a python dictionary
-    raw_dict = bf[day].dropna().to_dict()
+    raw_dict = []
+    try:
+        raw_dict = dn[day].dropna().to_dict()
+    except KeyError as e:
+        print ('I got a KeyError - reason "%s"' % str(e))
+    except IndexError as e:
+        print ('I got an IndexError - reason "%s"' % str(e))
 
     #if the menu is empty, return a string to notify there is no breakfast
     if bool(raw_dict) == False:
@@ -37,7 +43,13 @@ def get_today_breakfast(day):
 
 #same as above, with different date 
 def get_tmr_breakfast(day):
-    raw_dict = bf[day].dropna().to_dict()
+    raw_dict = []
+    try:
+        raw_dict = dn[day].dropna().to_dict()
+    except KeyError as e:
+        print ('I got a KeyError - reason "%s"' % str(e))
+    except IndexError as e:
+        print ('I got an IndexError - reason "%s"' % str(e))
     if bool(raw_dict) == False:
         return "There is no breakfast tomorrow." 
 
@@ -50,7 +62,14 @@ def get_tmr_breakfast(day):
     return string
 
 def get_tmr_dinner(day):
-    raw_dict = dn[day].dropna().to_dict()
+    raw_dict = []
+    try:
+        raw_dict = dn[day].dropna().to_dict()
+    except KeyError as e:
+        print ('I got a KeyError - reason "%s"' % str(e))
+    except IndexError as e:
+        print ('I got an IndexError - reason "%s"' % str(e))
+
     if bool(raw_dict) == False:
         return "There is no dinner tomorrow." 
 
@@ -67,7 +86,14 @@ def get_tmr_dinner(day):
     return string
 
 def get_today_dinner(day):
-    raw_dict = dn[day].dropna().to_dict()
+    raw_dict = []
+    try:
+        raw_dict = dn[day].dropna().to_dict()
+    except KeyError as e:
+        print ('I got a KeyError - reason "%s"' % str(e))
+    except IndexError as e:
+        print ('I got an IndexError - reason "%s"' % str(e))
+
     if bool(raw_dict) == False:
         return "There is no dinner today." 
     
@@ -82,3 +108,13 @@ def get_today_dinner(day):
     words_to_bold=['Meat', 'Side Dish', 'Special', 'Dessert']
 
     return string
+
+def test():
+    raw_dict = []
+    try:
+        raw_dict = dn['02/09/2019'].dropna().to_dict()
+    except KeyError as e:
+        print ('I got a KeyError - reason "%s"' % str(e))
+    except IndexError as e:
+            print ('I got an IndexError - reason "%s"' % str(e))
+    return raw_dict
