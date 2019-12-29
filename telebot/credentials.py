d3ca2ef746ff9bf7f639dd5f90f9601cb74dbc17
isbobby <bobbyclex8@gmail.com>
2019-12-29 15:16:09 +0800

    updated URL

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,4 @@
 bot_token = '676612820:AAHmVMr1Qkd0Cah7u1i7I-ByFBwb_pnGoO0'
-bot_user_name = "bot username"
-URL = "the heroku app link that we will create later"
+bot_user_name = "Eusoffbot"
+URL = "https://eusofftelebot.herokuapp.com/"
+
