fb1f33c443f62e08c2c6b6bbd5248b264fb63d91
isbobby <bobbyclex8@gmail.com>
2019-12-29 15:01:49 +0800

    webhook first commit

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
-from eusoffbot.main import bot, logger, time
+from flask import Flask, request
+import telegram
+from telebot.credentials import bot_token, bot_user_name,URL
+from telebot.mastermind import get_response
 
-while True:
-    try:
-        bot.polling()
-    # ConnectionError and ReadTimeout because of possible timout of the requests library
-    # TypeError for moviepy errors
-    # maybe there are others, therefore Exception
-    except Exception as e:
-        logger.error(e)
-        time.sleep(15)
+global bot
+global TOKEN
+
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    # get the chat_id to be able to respond to the same user
+    chat_id = update.message.chat.id
+    # get the message id to be able to reply to this specific message
+    msg_id = update.message.message_id
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+    # here we call our super AI
+    response = get_response(text)
+    # now just send the message back
+    # notice how we specify the chat and the msg we reply to
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    return 'ok'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    # we use the bot object to link the bot to our app which live
+    # in the link provided by URL
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    # something to let us know things work
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/')
+def index():
+    return '.'
+
+if __name__ == '__main__':
+    # note the threaded arg which allow
+    # your app to have more than one thread
+    app.run(threaded=True)
---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = '676612820:AAHmVMr1Qkd0Cah7u1i7I-ByFBwb_pnGoO0'
+bot_user_name = "bot username"
+URL = "the heroku app link that we will create later"
