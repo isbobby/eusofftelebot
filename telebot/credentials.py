3c939ac2060a42bae734511f2b2b907200b67c64
isbobby <bobbyclex8@gmail.com>
2020-01-05 12:05:59 +0800

    disable node_modules cache

--- telebot/credentials.py
+++
@@ -1,4 +0,0 @@
-bot_token = '676612820:AAHmVMr1Qkd0Cah7u1i7I-ByFBwb_pnGoO0'
-bot_user_name = "Eusoffbot"
-URL = "https://eusofftelebot.herokuapp.com/"
-
--- telebot/mastermind.py
+++
@@ -1,7 +0,0 @@
-def get_response(msg):
-    """
-    you can place your mastermind AI here
-    could be a very basic simple response like "hi"
-    or a complex LSTM network that generate appropriate answer
-    """
-    return "hi !"
