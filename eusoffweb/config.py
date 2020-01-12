e82882b1dffa6b66ec9993dae5b04c3807a8208b
isbobby <bobbyclex8@gmail.com>
2020-01-12 16:16:07 +0800

    added secret key

--- eusoffweb/config.py
+++ eusoffweb/config.py
@@ -4,7 +4,7 @@ import os
 #it is ignored during commits, change gitignore to enable commits
 
 class Config:
-    # SECRET_KEY = 'c9970460fc2c3ad324add53c94e3w3e4'
+    SECRET_KEY = 'c9970460fc2c3ad324add53c94e3w3e4'
     #SQLALCHEMY_DATABASE_URI = 'postgres://bobby:zmq,bs2008@localhost:5432/eusoffweb'
     #heroku uri
     SQLALCHEMY_DATABASE_URI = 'postgres://cwlecfheoxozqr:8f40918c5884a2e2f701ae1ea39e15e6ee0bee561f2373a0faa3318c05081c51@ec2-54-235-89-123.compute-1.amazonaws.com:5432/d11ekc90u8on7p'
