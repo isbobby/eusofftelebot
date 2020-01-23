a6ce61a0bc5b4cfd8690acd678992af4450af705
isbobby <bobbyclex8@gmail.com>
2020-01-24 00:56:24 +0800

    added exception handling for string concat.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 import telegram
-from eusoffbot.credentials import bot_token, bot_user_name, URL
+from eusoffbot.credentials import bot_token, bot_user_name, URL, reset_key
 from eusoffbot.mastermind import getResponse
 from eusoffweb import create_app
 global bot
 global TOKEN
+global RESETKEY
+
+RESETKEY = reset_key
 
 TOKEN = bot_token
 
@@ -28,8 +31,12 @@ def respond():
         msg_id = update.message.message_id
         user = update.message.from_user 
         text = update.message.text.encode('utf-8').decode()
-        info = "got text message: " + text + " from " + user.username
-        print(info)
+        try:
+            info = "got text message: " + text + " from " + user.username
+            print(info)
+        except:
+            print("no message is received")
+
         response = getResponse(update.message)
         
         if response.has_markup:
@@ -45,6 +52,10 @@ def respond():
 
     return 'ok'
 
+@app.route('/{}'.format(RESETKEY), methods=['POST'])
+def reset():
+    return 'ok'
+
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
     # we use the bot object to link the bot to our app which live
@@ -56,7 +67,19 @@ def set_webhook():
     else:
         return "webhook setup failed"
 
+@app.route('/resetupdate', methods=['GET','POST'])
+def reset_update():
+    """
+    Really a temprorary method to keep the update from flooding
+    """
+    s = bot.setWebhook('{URL}{RESET}'.format(URL=URL, RESET=RESETKEY))
+    if s:
+        return "reset hook setup ok"
+    else:
+        return "reset hook setup failed"
+
 if __name__ == '__main__':
     # note the threaded arg which allow
     # your app to have more than one thread
     app.run(threaded=True, debug=True)
+
--- eusoffbot/credentials.py
+++ eusoffbot/credentials.py
@@ -1,5 +1,5 @@
 bot_token = '676612820:AAHmVMr1Qkd0Cah7u1i7I-ByFBwb_pnGoO0'
 bot_user_name = "Eusoffbot"
 URL = "https://eusofftelebot.herokuapp.com/"
-
+reset_key = 'resetkey2H782F9H08F2N9UBPVSHDBDJASJDONsSDHS90D'
 
